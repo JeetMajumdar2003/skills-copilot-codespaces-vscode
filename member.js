edbed330daf60a3273a67bc1fd83e09364bacf2d
Jeet Majumdar <109456123+JeetMajumdar2003@users.noreply.github.com>
2023-07-18 13:55:59 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,12 @@
-function member(){
-    return {
-        restrict: 'E',
-        templateUrl: 'modu les/ski I Is/ views/member. html',
-        controller: 'SkillsMemberControlleri',
-        controllerAs:'vm',
-        bindToController: true,
-        scope: {
-        member: '='
-        }
-    };
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
 }
